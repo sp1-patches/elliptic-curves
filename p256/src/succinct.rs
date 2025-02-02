0e69a892e7a3adc09a710be7a2aa83448a37530f
rkm0959 <rkm0959@gmail.com>
2025-02-02 10:09:58 -0800

    minor comment fix

--- p256/src/succinct.rs
+++ p256/src/succinct.rs
@@ -69,7 +69,6 @@ mod affine {
         }
 
         /// # Panics 
-        /// - if the point is the identity point.
         /// - if we have non canon represntations of the field elements.
         pub(crate) fn field_elements(&self) -> (FieldElement, FieldElement) {
             if self.is_identity().into() {
